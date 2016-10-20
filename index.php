fc2bfd5c4ef8e069b72853fd5123cf29c656b943
afterlogic-support <support@afterlogic.com>
2016-10-20 18:37:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2002-2013 AfterLogic Corp. (www.afterlogic.com)
- * Distributed under the terms of the license described in LICENSE
- *
- */
-
 class_exists('CApi') or die();
 
 CApi::Inc('common.plugins.change-password');
